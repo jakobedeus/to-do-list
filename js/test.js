586fad3f937fff885194955abd530eed7193f5f5
jakobedeusb <jakob@edeus.segit config --global user.email jakob@edeus.segit config --global user.email>
2019-03-10 18:43:53 +0100

    add new files

--- js/test.js
+++ js/test.js
@@ -4,9 +4,40 @@ function toDoListClass(activity, status) {
     this.Status = status;
 }
 
+function reset() {
+    let item1 = new toDoListClass("Go to the gym", false);
+        let item2 = new toDoListClass("Study Javascript", false);
+        let item3 = new toDoListClass("Watch netflix", false);
+        let item4 = new toDoListClass("Grocery shopping", false);
+    
+        let list = [item1, item2, item3, item4];
+    
+        localStorage.setItem("toDoList", JSON.stringify(list));
+        printList();
+}
+
+function checkList() {
+
+    if(localStorage.getItem("toDoList") === null) {
+        let item1 = new toDoListClass("Go to the gym", false);
+        let item2 = new toDoListClass("Study Javascript", false);
+        let item3 = new toDoListClass("Watch netflix", false);
+        let item4 = new toDoListClass("Grocery shopping", false);
+    
+        let list = [item1, item2, item3, item4];
+    
+        localStorage.setItem("toDoList", JSON.stringify(list));
+        printList();
+
+    } else {
+        printList();
+    }
+}
+
 // Function to loop list, either to done list, or not done list. Reset lists before looping to avoid duplication. 
 function printList() {
-    let listFromStorage = JSON.parse(localStorage.getItem("toDoList"));
+    var listFromStorage = JSON.parse(localStorage.getItem("toDoList"));
+    
     document.getElementById("toDoList").innerHTML = "";
     document.getElementById("doneList").innerHTML = "";
 
@@ -18,19 +49,6 @@ function printList() {
             document.getElementById("doneList").innerHTML += "<li id='" + [i] + "'><div class='listInfo' onclick='undoTask(" + [i] + ")' ><input type='checkbox' id='checkbox" + [i] + "'checked></input><label for='checkbox" + i + "'>" + listFromStorage[i].Activity + "</label></div><div class='listRemove'><button id='" + [i] + "' onclick='removeTask(" + [i] + ")' class='remove-btn'><i class='fas fa-trash'></i></button></div></li>";
         }
     }
-}
-
-function toDoList() {
-    let item1 = new toDoListClass("Go to the gym", false);
-    let item2 = new toDoListClass("Study Javascript", false);
-    let item3 = new toDoListClass("Watch netflix", false);
-    let item4 = new toDoListClass("Grocery shopping", false);
-
-    let list = [item1, item2, item3, item4];
-
-    localStorage.setItem("toDoList", JSON.stringify(list));
-    // Call function which loops array from localstorage.
-    printList();
 }
 
 function sortASC() {
